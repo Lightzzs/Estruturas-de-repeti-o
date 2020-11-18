95a5790af6c1627dc110fd51f937ec6ed449b289
VINICIUS <marvinicius78910@gmail.com>
2020-11-18 18:41:51 -0200

    first commit

---
+++ Projeto 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Usuario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string usuario;
+            string senha;
+            do{
+                Console.WriteLine("Digite seu login de usúario");
+                usuario = Console.ReadLine();
+
+
+                Console.WriteLine("Digite sua senha de login");
+                senha = Console.ReadLine();
+
+                if(usuario == senha){
+                    Console.WriteLine("sua senha é o nome de usuario são os mesmos");
+                }
+
+            }while(usuario == senha );
+                 Console.WriteLine("Login Concluido Com Sucesso");
+        }
+    }
+}   
+    
+        
+    
+
